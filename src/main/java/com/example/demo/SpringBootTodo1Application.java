45722f8e95bec1ee2f90ced130f49bc9b3abd769
YumaMaruyama <83486993+YumaMaruyama@users.noreply.github.com>
2021-10-06 16:15:02 +0900

    jarファイル作成

--- src/main/java/com/example/demo/SpringBootTodo1Application.java
+++ src/main/java/com/example/demo/SpringBootTodo1Application.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootTodo1Application {
+public class SpringBootTodo1Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootTodo1Application.class, args);
 	}
 
+	@Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(SpringBootTodo1Application.class);
+	  }
 }
